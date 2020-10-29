19290d9a6f34890ebf8c8236d6f2a87c17e0df50
CrayJThiemsert <cray.j.thiemsert@gmail.com>
2020-10-29 14:51:56 +0700

    - can show itemdata but not match to item uid yet.

--- lib/ui/topic/widgets/items_menu.dart
+++ lib/ui/topic/widgets/items_menu.dart
@@ -71,18 +71,23 @@ class _ItemsMenuState extends State<ItemsMenu> {
             final items = state.items;
             print('items.length[${items.length}]');
             if(items.length > 0) {
+              int currentSwiperIndex = 0;
               return Container(
                 height: displayHeight(context) * 0.6,
                 width: displayWidth(context),
                 color: Colors.lightGreen[50],
                 child: Swiper(
+                  onIndexChanged: (index)  {
+                    currentSwiperIndex = index;
+                    print('current swiper index[${index}] currentSwiperIndex[${currentSwiperIndex}]');
+                  },
                   itemBuilder: (BuildContext context, int index) {
                     // final item = (items.length == 0) ? templateItems[index] : items[index];
                     final item = items[index];
 
                     print('**Caller index[${index}] - item.index[${item.index}] - item.uid[${item.uid}]');
                     // Get itemdata list stream
-                    context.bloc<ItemDatasBloc>().add(LoadItemDatas(categoryUid, partUid, topicUid, topic, item));
+                    // context.bloc<ItemDatasBloc>().add(LoadItemDatas(categoryUid, partUid, topicUid, topic, item));
 
                     item.topic = topic;
                     return Column(
@@ -187,6 +192,8 @@ class _ItemsMenuState extends State<ItemsMenu> {
     )
     );
     for(int i=2;i<item.headers.length;i++){
+      // call a ItemData load stream
+      context.bloc<ItemDatasBloc>().add(LoadItemData(categoryUid, partUid, topicUid, topic, item, item.headers[i].uid));
       switch(i % 2) {
         case 0: { // left
           print('left[${i}] ${item.headers[i].uid}');
@@ -239,8 +246,6 @@ class _ItemsMenuState extends State<ItemsMenu> {
     ItemData itemData = getItemData(item.headers[i], item);
     print('<<<header[${item.headers[i].uid} -> From Item Bloc list${itemData}>>> item no.${item.index} [${item.uid}] ');
 
-    // call a ItemData load stream
-    // context.bloc<ItemDatasBloc>().add(LoadItemData(categoryUid, partUid, topicUid, topic, item, itemData.uid));
     ItemData itemDataStream = ItemData(uid: itemData.uid);
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
@@ -269,14 +274,15 @@ class _ItemsMenuState extends State<ItemsMenu> {
                   ),
                 ),
                 BlocBuilder<ItemDatasBloc, ItemDatasState>(
-                  buildWhen: (previous, current) {
-                    (previous.itemDataUid != current.itemDataUid);
+                  // buildWhen: (previous, current) {
+                  //   print('*previous.itemDataUid[${previous.itemDataUid}] - current.itemDataUid[${current.itemDataUid}]');
+                  //   (previous.itemDataUid != current.itemDataUid);
                   //   if(previous is ItemDataLoaded && current is ItemDataLoaded) {
                   //     return (previous.itemData != current.itemData);
                   //   } else {
                   //     return false;
                   //   }
-                    },
+                  //   },
                   builder: (context, state) {
                     if (state is ItemDatasLoading) {
                       print('ItemDatasLoading...');
@@ -290,14 +296,27 @@ class _ItemsMenuState extends State<ItemsMenu> {
                           fontWeight: FontWeight.bold,
                         ),
                       );
-                    // } else if(state is ItemDataLoaded) {
-                    } else if(state is ItemDatasLoaded) {
-                      print('ItemDatasLoaded...');
-                      print('#### item.uid[${item.uid}]=> i[${i}] ${item.headers[i].uid}  - state.itemDatas.length[${state.itemDatas.length}]');
-                      itemDataStream = getItemDataByUid(item.uid, item.headers[i].uid, state.itemDatas);
+                    } else if(state is ItemDataLoaded) {
+                      print('ItemDataLoaded...');
+                    // } else if(state is ItemDatasLoaded) {
+                    //   print('ItemDatasLoaded...');
+                    //   print('#### item.uid[${item.uid}]=> i[${i}] ${item.headers[i].uid}  - state.itemDatas.length[${state.itemDatas.length}]');
+                    //   itemDataStream = getItemDataByUid(item.uid, item.headers[i].uid, state.itemDatas);
+                      print('#### item.uid[${item.uid}]=> i[${i}] ${item.headers[i].uid}  - state.itemDatas[${state.itemData}]');
+                      itemDataStream = state.itemData;
                       print('>>>itemDataStream=${itemDataStream}');
+                      String value = '';
+                      // if(itemDataStream.id == null) {
+                      //   value = 'ld-${itemData.value}';
+                      // } else {
+                      //   value = 'st-${itemDataStream.value}';
+                      // }
+                      if(item.headers[i].uid == itemDataStream.uid) {
+                        value = 'st-${itemDataStream.value}';
+                      }
+
                       return Text(
-                        '${itemDataStream.value}',
+                        '${value}',
                         style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.bold,
@@ -311,6 +330,10 @@ class _ItemsMenuState extends State<ItemsMenu> {
           ),
           onPressed: () {
             print('hit ${itemDataStream.uid}!!');
+            String value = '';
+            if(item.headers[i].uid == itemDataStream.uid) {
+              value = 'st-${itemDataStream.value}';
+            }
 
             // BlocBuilder<ItemDatasBloc, ItemDatasState>(
             //     builder: (context, state) {
@@ -331,7 +354,7 @@ class _ItemsMenuState extends State<ItemsMenu> {
             //           'OK',
             //         );
             //       } else if(state is ItemDataLoaded) {
-                    print('>>>itemDataStream.uid=${itemDataStream.uid}');
+                    print('onpressed >>>itemDataStream.uid=${itemDataStream.uid}');
                     DialogUtils().showInputDialog(
                       context: context,
                       title: 'Input ${item.headers[i].name} Data',
@@ -339,7 +362,7 @@ class _ItemsMenuState extends State<ItemsMenu> {
                       noText: 'Cancel',
                       inputType: item.headers[i].inputType,
                       key: itemDataStream.uid,
-                      content: itemDataStream.value,
+                      content: value,
                       item: item,
                       itemData: itemDataStream,
                       itemDatasBloc: itemDatasBloc,
